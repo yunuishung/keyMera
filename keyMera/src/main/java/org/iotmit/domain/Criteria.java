0bf0c15d11be5f1af160f723fcbb92f5b720b805
mit <mit@DESKTOP-5ULJNMC>
2020-04-17 12:21:50 +0900

    modify : log4jdbc.log4j2.properties error report

--- keyMera/src/main/java/org/iotmit/domain/Criteria.java
+++ keyMera/src/main/java/org/iotmit/domain/Criteria.java
@@ -9,14 +9,14 @@ import lombok.ToString;
 @ToString
 public class Criteria {
 	private int pageNum;
-	private int amount;//한페이지에 보여줄 데이터
+	private int amount;
 	
 	public Criteria(int pageNum,int amount) {
-		this.pageNum=pageNum;
+		this.pageNum = pageNum;
 		this.amount = amount;
 	}
 	
-	public Criteria() {//별다른 설정이 없으면 초기값 1하고 10을 가지도록 설정
-		this(1,10);//1페이지에 10개
+	public Criteria() {
+		this(1,10);
 	}
 }
